167d8f9c5bdea915c529146656f81ee7d5fbbe6c
alinaama-2050 <92233230+alinaama-2050@users.noreply.github.com>
2022-01-23 23:19:16 +0100

    Create Projet 5.py

---
+++ Projet 5/Projet 5.py
@@ -0,0 +1,208 @@
+# ******************************************
+#
+# Project : Customer Segmentation
+# Project 5
+# http://localhost:8888/notebooks/CustomerSegmentation.ipynb
+# *******************************************
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# 1. Load Data
+df = pd.read_excel('/data/2020AnnualSales.xlsx', sheet_name='Sales')
+
+# 2.
+print(df.shape)
+print(df.head())
+
+# 2. Data Clean-Up
+df.loc[df['SBQQ__Quantity__c'] <= 0].shape
+print(df.shape)
+
+#### - Total Sales
+df['Sales'] = df['SBQQ__Quantity__c'] * df['SBQQ__NetPrice__c']
+
+print(df.head())
+
+#### - Per Customer Data
+
+customer_df = df.groupby('SBQQ__Account__c').agg({
+    'Sales': sum,
+    'Name': lambda x: x.nunique()
+})
+#### - Total Sales
+customer_df.columns = ['TotalSales', 'OrderCount']
+customer_df['AvgOrderValue'] = customer_df['TotalSales']/customer_df['OrderCount']
+
+print(customer_df.head(15))
+
+print(customer_df.describe())
+
+#### - Ranking
+rank_df = customer_df.rank(method='first')
+print(rank_df.head(15))
+
+
+#### - Normalize
+normalized_df = (rank_df - rank_df.mean()) / rank_df.std()
+
+
+print(normalized_df.head(15))
+print(normalized_df.describe())
+
+# 3. Customer Segmentation via K-Means Clustering
+
+from sklearn.cluster import KMeans
+
+#### - K-Means Clustering
+
+kmeans = KMeans(n_clusters=4).fit(normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']])
+
+print(kmeans)
+
+kmeans.labels_
+
+kmeans.cluster_centers_
+
+four_cluster_df = normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']].copy(deep=True)
+four_cluster_df['Cluster'] = kmeans.labels_
+four_cluster_df.head()
+
+four_cluster_df.groupby('Cluster').count()['TotalSales']
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['TotalSales'],
+    c='blue'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['TotalSales'],
+    c='red'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['TotalSales'],
+    c='orange'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['TotalSales'],
+    c='green'
+)
+
+plt.title('TotalSales vs. OrderCount Clusters')
+plt.xlabel('Order Count')
+plt.ylabel('Total Sales')
+
+plt.grid()
+plt.show()
+
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['AvgOrderValue'],
+    c='blue'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['AvgOrderValue'],
+    c='red'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['AvgOrderValue'],
+    c='orange'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['OrderCount'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['AvgOrderValue'],
+    c='green'
+)
+
+plt.title('AvgOrderValue vs. OrderCount Clusters')
+plt.xlabel('Order Count')
+plt.ylabel('Avg Order Value')
+
+plt.grid()
+plt.show()
+
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['TotalSales'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['AvgOrderValue'],
+    c='blue'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['TotalSales'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['AvgOrderValue'],
+    c='red'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['TotalSales'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['AvgOrderValue'],
+    c='orange'
+)
+
+plt.scatter(
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['TotalSales'],
+    four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['AvgOrderValue'],
+    c='green'
+)
+
+plt.title('AvgOrderValue vs. TotalSales Clusters')
+plt.xlabel('Total Sales')
+plt.ylabel('Avg Order Value')
+
+plt.grid()
+plt.show()
+
+
+#### - Selecting the best number of clusters
+
+from sklearn.metrics import silhouette_score
+
+for n_cluster in [4, 5, 6, 7, 8]:
+    kmeans = KMeans(n_clusters=n_cluster).fit(
+        normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']]
+    )
+    silhouette_avg = silhouette_score(
+        normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']],
+        kmeans.labels_
+    )
+
+    print('Silhouette Score for %i Clusters: %0.4f' % (n_cluster, silhouette_avg))
+
+
+#### - Interpreting Customer Segments
+
+kmeans = KMeans(n_clusters=4).fit(
+    normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']]
+)
+
+four_cluster_df = normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']].copy(deep=True)
+four_cluster_df['Cluster'] = kmeans.labels_
+
+four_cluster_df.head(15)
+
+kmeans.cluster_centers_
+high_value_cluster = four_cluster_df.loc[four_cluster_df['Cluster'] == 2]
+high_value_cluster.head()
+customer_df.loc[high_value_cluster.index].describe()
+pd.DataFrame(
+    df.loc[
+        df['SBQQ__Account__c'].isin(high_value_cluster.index)
+    ].groupby('Description__c').count()[
+        'SBQQ__ProductName__c'
+    ].sort_values(ascending=False).head()
+)
